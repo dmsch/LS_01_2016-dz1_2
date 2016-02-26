e6e3f6e6256b12735a0c89b6131eec23c414b14d
dmsch <dschukin@mail.ru>
2016-02-26 11:58:55 +0300

    DZ-1 Phase-2 Update

--- app/js/contact-me.js
+++ app/js/contact-me.js
@@ -4,15 +4,170 @@ $(document).ready(function() {
 		$('input, textarea').placeholder();
 	};
 
-	var bPopup;
 
-	$(".button-send").on("click", function(event) {
-		event.preventDefault();
-
-		bPopup = $(".element-to-popup").bPopup();
+	// Tooltipster ===========================================================
+	var tooltipsterContactName = $("#contactName").tooltipster({
+		position: "left",
+		multiple: true,
+		trigger: "custom",
+		theme: "tooltipster-red",
+		content: "введите имя"
 	});
 
-	$(".popup-close-button").on("click", function (event) {
-		bPopup.close();
+	var tooltipsterContactEmail = $("#contactEmail").tooltipster({
+		position: "right",
+		multiple: true,
+		trigger: "custom",
+		theme: "tooltipster-red",
+		content: "введите email"
+	});
+
+	var tooltipsterInputMessage = $("#inputMessage").tooltipster({
+		position: "left",
+		multiple: true,
+		trigger: "custom",
+		theme: "tooltipster-red",
+		content: "ваш вопрос"
+	});
+
+	var tooltipsterCapchaInput = $("#capchaInput").tooltipster({
+		position: "right",
+		multiple: true,
+		trigger: "custom",
+		theme: "tooltipster-red",
+		content: "код капчи"
+	});
+
+	$(".button-send").on('click', function(event) {
+		event.preventDefault();
+		
+		// Validate input fields ===============================================
+		var myNewProjectValidation = true;
+
+		if ($("#contactName").val() == "") {
+			myNewProjectValidation = false;
+			tooltipsterContactName[0].show();
+			$("#contactName").css({
+				border:"2px solid #e0ad9a",
+			});
+		} else {
+			tooltipsterContactName[0].hide();
+		}
+
+		if ($("#contactEmail").val() == "") {
+			myNewProjectValidation = false;
+			tooltipsterContactEmail[0].show();
+			$("#contactEmail").css({
+				border:"2px solid #e0ad9a",
+			});
+		} else {
+			tooltipsterContactEmail[0].hide();
+		}
+
+		if ($("#inputMessage").val() == "") {
+			myNewProjectValidation = false;
+			tooltipsterInputMessage[0].show();
+			$("#inputMessage").css({
+				border:"2px solid #e0ad9a",
+			});
+		} else {
+			tooltipsterInputMessage[0].hide();
+		}
+
+		if ($("#capchaInput").val() == "") {
+			myNewProjectValidation = false;
+			tooltipsterCapchaInput[0].show();
+			$("#capchaInput").css({
+				border:"2px solid #e0ad9a",
+			});
+		} else {
+			tooltipsterCapchaInput[0].hide();
+		}
+
+		$("#contactName").on('focus', function(event) {
+			tooltipsterContactName[0].hide();
+			$("#contactName").css({
+				border:"1px solid #d6d5d3",
+			});
+		});
+		$("#contactName").on('blur', function(event) {
+			if ($(this).val() == "") {
+				tooltipsterContactName[0].show();
+				$("#contactName").css({
+					border:"2px solid #e0ad9a",
+				});
+			};
+		});
+		$("#contactEmail").on('focus', function(event) {
+			tooltipsterContactEmail[0].hide();
+			$("#contactEmail").css({
+				border:"1px solid #d6d5d3",
+			});
+		});
+		$("#contactEmail").on('blur', function(event) {
+			if ($(this).val() == "") {
+				tooltipsterContactEmail[0].show();
+				$("#contactEmail").css({
+					border:"2px solid #e0ad9a",
+				});
+			};
+		});
+		$("#inputMessage").on('focus', function(event) {
+			tooltipsterInputMessage[0].hide();
+			$("#inputMessage").css({
+				border:"1px solid #d6d5d3",
+			});
+		});
+		$("#inputMessage").on('blur', function(event) {
+			if ($(this).val() == "") {
+				tooltipsterInputMessage[0].show();
+				$("#inputMessage").css({
+					border:"2px solid #e0ad9a",
+				});
+			};
+		});
+		$("#capchaInput").on('focus', function(event) {
+			tooltipsterCapchaInput[0].hide();
+			$("#capchaInput").css({
+				border:"1px solid #d6d5d3",
+			});
+		});
+		$("#capchaInput").on('blur', function(event) {
+			if ($(this).val() == "") {
+				tooltipsterCapchaInput[0].show();
+				$("#capchaInput").css({
+					border:"2px solid #e0ad9a",
+				});
+			};
+		});
+		// End of Validate input fields ===============================================
+	});
+
+	$(".button-clear").on('click', function(event) {
+		event.preventDefault();
+		
+		tooltipsterContactName[0].hide();
+		$("#contactName").css({
+			border:"2px solid #d6d5d3",
+		});
+		$("#contactName").val("");
+
+		tooltipsterContactEmail[0].hide();
+		$("#contactEmail").css({
+			border:"2px solid #d6d5d3",
+		});
+		$("#contactEmail").val("");
+
+		tooltipsterInputMessage[0].hide();
+		$("#inputMessage").css({
+			border:"2px solid #d6d5d3",
+		});
+		$("#inputMessage").val("");
+
+		tooltipsterCapchaInput[0].hide();
+		$("#capchaInput").css({
+			border:"2px solid #d6d5d3",
+		});
+		$("#capchaInput").val("");
 	});
 });
--- app/js/my-portfolio.js
+++ app/js/my-portfolio.js
@@ -4,12 +4,37 @@ $(document).ready(function() {
 		$('input, textarea').placeholder();
 	};
 
+	// bPopup =======================================
 	var bPopup, bPopupOk;
 
 	$(".add-new-project-link").on("click", function (event) {
 		event.preventDefault();
 
-		bPopup = $(".add-project-popup").bPopup();
+		bPopup = $(".add-project-popup").bPopup({
+			onClose: function () {
+				tooltipsterProjectName[0].hide();
+				$("#projectName").css({
+					border:"1px solid #d6d5d3",
+				});
+				tooltipsterFileName[0].hide();
+				$(".fileform").css({
+					border:"2px solid #d6d5d3",
+				});
+				tooltipsterProjectURL[0].hide();
+				$("#projectURL").css({
+					border:"1px solid #d6d5d3",
+				});
+				tooltipsterProjectDesc[0].hide();
+				$("#projectDescMessage").css({
+					border:"1px solid #d6d5d3",
+				});
+			}
+		});
+
+		$("#projectName").val("");
+		$("#fileformlabel").val("");
+		$("#projectURL").val("");
+		$("#projectDescMessage").val("");
 	});
 
 	$(".popup-close-button").on("click", function (event) {
@@ -18,17 +43,66 @@ $(document).ready(function() {
 
 	$(".button-send").on("click", function (event) {
 		event.preventDefault();
-		bPopupOk = $(".popup-ok").bPopup({
-			onClose: function () {bPopup.close();}
-		});
-	})
+
+		// Validate input fields ===============================================
+		var myNewProjectValidation = true;
+
+		if ($("#projectName").val() == "") {
+			myNewProjectValidation = false;
+			tooltipsterProjectName[0].show();
+			$("#projectName").css({
+				border:"2px solid #e0ad9a",
+			});
+		} else {
+			tooltipsterProjectName[0].hide();
+		}
+
+		if ($("#fileformlabel").val() == "") {
+			myNewProjectValidation = false;
+			tooltipsterFileName[0].show();
+			$(".fileform").css({
+				border:"2px solid #e0ad9a",
+			});
+		} else {
+			tooltipsterFileName[0].hide();
+		}
+
+		if ($("#projectURL").val() == "") {
+			myNewProjectValidation = false;
+			tooltipsterProjectURL[0].show();
+			$("#projectURL").css({
+				border:"2px solid #e0ad9a",
+			});
+		} else {
+			tooltipsterProjectURL[0].hide();
+		}
+
+		if ($("#projectDescMessage").val() == "") {
+			myNewProjectValidation = false;
+			tooltipsterProjectDesc[0].show();
+			$("#projectDescMessage").css({
+				border:"2px solid #e0ad9a",
+			});
+		} else {
+			tooltipsterProjectDesc[0].hide();
+		}
+		// End of Validate input fields ===============================================
+
+		if (myNewProjectValidation) {
+			bPopupOk = $(".popup-ok").bPopup({
+				onClose: function () {bPopup.close();}
+			});
+		}
+		
+	});
 
 	$(".popup-close-button.popupOk").on("click", function (event) {
 		bPopupOk.close();
 		bPopup.close();
 	});
+	// End of bPopup =======================================
 
-
+	// File upload ====================================
 	$("#upload").on("change", function () {
 		var myStr = $("#upload").val();
 
@@ -40,6 +114,104 @@ $(document).ready(function() {
 	    };						
 
 		$("#fileformlabel").val(myStr.slice(i));
+
+		if ($("#fileformlabel").val() != "") {
+			tooltipsterFileName[0].hide();
+			$(".fileform").css({
+				border:"2px solid #d6d5d3",
+			});
+		}; 
 	});
+	// End of File upload ====================================
+
+	// Tooltipster ===========================================================
+	var tooltipsterProjectName = $("#projectName").tooltipster({
+		position: "left",
+		multiple: true,
+		trigger: "custom",
+		theme: "tooltipster-red",
+		content: "введите название"
+	});
+
+	var tooltipsterFileName = $("#upload").tooltipster({
+		position: "left",
+		multiple: true,
+		trigger: "custom",
+		theme: "tooltipster-red",
+		content: "изображение"
+	});
+
+	var tooltipsterProjectURL = $("#projectURL").tooltipster({
+		position: "left",
+		multiple: true,
+		trigger: "custom",
+		theme: "tooltipster-red",
+		content: "ссылка на проект"
+	});
+
+	var tooltipsterProjectDesc = $("#projectDescMessage").tooltipster({
+		position: "left",
+		multiple: true,
+		trigger: "custom",
+		theme: "tooltipster-red",
+		content: "описание проекта"
+	});
+
+	$("#projectName").on('focus', function(event) {
+		tooltipsterProjectName[0].hide();
+		$("#projectName").css({
+			border:"1px solid #d6d5d3",
+		});
+	});
+	$("#projectName").on('blur', function(event) {
+		if ($(this).val() == "") {
+			tooltipsterProjectName[0].show();
+			$("#projectName").css({
+				border:"2px solid #e0ad9a",
+			});
+		};
+	});
+
+/*	$("#fileformlabel").on('change', function(event) {
+		tooltipsterFileName[0].hide();
+	});
+	$("#fileformlabel").on('blur', function(event) {
+		if ($(this).val() == "") tooltipsterFileName[0].show();
+	});*/
+
+	$("#projectURL").on('focus', function(event) {
+		tooltipsterProjectURL[0].hide();
+		$("#projectURL").css({
+			border:"1px solid #d6d5d3",
+		});
+	});
+	$("#projectURL").on('blur', function(event) {
+		if ($(this).val() == "") {
+			tooltipsterProjectURL[0].show();
+			$("#projectURL").css({
+				border:"2px solid #e0ad9a",
+			});
+		};
+	});
+
+	$("#projectDescMessage").on('focus', function(event) {
+		tooltipsterProjectDesc[0].hide();
+		$("#projectDescMessage").css({
+			border:"1px solid #d6d5d3",
+		});
+	});
+	$("#projectDescMessage").on('blur', function(event) {
+		if ($(this).val() == "") {
+			tooltipsterProjectDesc[0].show();
+			$("#projectDescMessage").css({
+				border:"2px solid #e0ad9a",
+			});
+		};
+	});
+
+
+	// End of Tooltipster ===========================================================
+
+	
 
 });
